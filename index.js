fa8e13532be4c738db680c7e1c1cb2bf470baf1f
Krishna <52949157+krishna-hub@users.noreply.github.com>
2021-03-20 20:04:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+setInterval(displayclock, 1);
+
+function displayclock()
+{
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+
+    if(hrs>12)
+    {
+      hrs = hrs-12;
+    }
+    if(hrs ==0)
+    {
+      hrs = 12;
+    }
+
+    document.getElementById("clock").innerHTML = hrs +":"+min+":"+sec;
+
+}
